b0398fcc08e9ea2c4d517562ad1bf05b181bf8f7
Bttaleb <btalebg@gmail.com>
2026-02-24 00:38:28 -0500

    Second
    updated

---
+++ card-game-2/Script/CardDatabase.gd
@@ -0,0 +1,8 @@
+const CARDS = {
+	"Pawn" : [5,0],
+	"Knight" : [10,5],
+	"Bishop" : [5,5],
+	"Rook" : [0,10],
+	"Queen" : [25,25],
+	"King" : [10,25]	
+}
--- new-game-project/tasks/lessons.md
+++
@@ -1,7 +0,0 @@
-# Lessons Learned
-
-## Godot Patterns
-- (to be filled as we build)
-
-## Mistakes & Corrections
-- (to be filled as we iterate)
